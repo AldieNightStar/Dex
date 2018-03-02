1b9895a7e616e9145567d1d6ee5b41b378f8483a
HaxiDenti <aldie.nightstar@gmail.com>
2018-03-02 08:24:12 +0200

    Add files via upload

--- src/main/java/com/fox/Dex/Main.java
+++ src/main/java/com/fox/Dex/Main.java
@@ -3,9 +3,14 @@ package com.fox.Dex;
 import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
 
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.nio.file.StandardOpenOption;
 
 public class Main {
 
@@ -14,13 +19,15 @@ public class Main {
     public static void main(String[] args) {
 
         if (args.length < 1){
-            System.out.println("args: [zipname | txtname]");
+            System.out.println("USAGE: dex [zipname | txtname | .]");
             System.out.println("Examples:");
-            System.out.println("thisapp tegra.zip");
+            System.out.println("thisapp tegra.zip -- To encrypt");
             System.out.println("or");
-            System.out.println("thisapp tegra.txt");
+            System.out.println("thisapp tegra.txt -- To decrypt");
             System.out.println("");
             System.out.println("To encrypt data, just add \"*\" as second argument");
+            System.out.println("");
+            System.out.println("To decrypt from clipboard, type: dex .");
             return;
         }
 
@@ -32,6 +39,13 @@ public class Main {
 
 
         String filename = args[0];
+        // If it clipboard
+        if (filename.equals(".")){
+            String newFileName = "clipboard.txt";
+            doSaveFileFromClipBoardAs(newFileName);
+            filename = newFileName;
+        }
+
         boolean textFile = isItTextFile(filename);
         boolean zipFile = isZipFile(filename);
 
@@ -92,6 +106,9 @@ public class Main {
             }
 
             Files.write(newFile.toPath(), base64.getBytes());
+
+            System.out.println("File saved as "+newFile.getName()+"!");
+            System.out.println("You can open it and copy context to clipboard");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -119,6 +136,19 @@ public class Main {
             e.printStackTrace();
         }
 
+    }
+
+    private static void doSaveFileFromClipBoardAs(String name){
+        File file = new File(name);
+        try {
+            if (file.exists()) file.delete();
+            Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+            byte[] bytes = ((String) clipboard.getData(DataFlavor.stringFlavor)).getBytes();
+            Files.write(file.toPath(), bytes, StandardOpenOption.CREATE, StandardOpenOption.SPARSE);
+            System.out.println("File created with name "+name+" from clipboard!");
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     private static String swapSubstrings(String str, String from, String to){
